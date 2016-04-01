91836feceb76f30ccc13df939d1375757b94f84c
KangboLu <redstonegranola@gmail.com>
2016-04-01 09:19:06 -0700

    Making changes for slider of News section

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-    //  $('.news-slide-box').slick({
+      $('.news-slide-box').slick({
+        dots: true,
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 2,
         autoplay: true,
-        autoplaySpeed: 2000
+        autoplaySpeed: 2500
       });
 });
 
