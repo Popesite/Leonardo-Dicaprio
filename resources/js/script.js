582ae81ac11bb679de735d4396bcf574bc0a30aa
KangboLu <redstonegranola@gmail.com>
2016-03-31 18:52:07 -0700

    Adding more effect to scroll for later

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,10 @@
- $(document).ready(function(){
-      $('.news-slide-box').slick({
+$(document).ready(function(){
+    //  $('.news-slide-box').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000
       });
 });
 
